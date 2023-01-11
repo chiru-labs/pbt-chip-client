5c13d5baf50de18be704adef7294ec9bcffbc787
Robert McMenemy <total.trance.network@gmail.com>
2023-01-11 13:49:42 +0000

    Refactor promise in xdd to avoid race conditions

--- src/kong/index.ts
+++ src/kong/index.ts
@@ -104,29 +104,31 @@ export function parseKeys(payload: string) {
 
 async function triggerScan({ reqx, rpId }: { reqx: string; rpId: string }) {
   try {
-    const xdd = await navigator.credentials.get({
-      publicKey: {
-        allowCredentials: [
-          {
-            id: fromHexString(reqx),
-            transports: ["nfc"],
-            type: "public-key",
-          },
-        ],
-        challenge: new Uint8Array([
-          113, 241, 176, 49, 249, 113, 39, 237, 135, 170, 177, 61, 15, 14, 105,
-          236, 120, 140, 4, 41, 65, 225, 107, 63, 214, 129, 133, 223, 169, 200,
-          21, 88,
-        ]),
-        rpId,
-        timeout: 60000,
-        userVerification: "discouraged",
-      },
-    });
-
-    if (xdd?.type === "public-key") {
-      return (xdd.response as AuthenticatorAssertionResponse).signature;
-    }
+    await navigator.credentials
+      .get({
+        publicKey: {
+          allowCredentials: [
+            {
+              id: fromHexString(reqx),
+              transports: ["nfc"],
+              type: "public-key",
+            },
+          ],
+          challenge: new Uint8Array([
+            113, 241, 176, 49, 249, 113, 39, 237, 135, 170, 177, 61, 15, 14,
+            105, 236, 120, 140, 4, 41, 65, 225, 107, 63, 214, 129, 133, 223,
+            169, 200, 21, 88,
+          ]),
+          rpId,
+          timeout: 60000,
+          userVerification: "discouraged",
+        },
+      })
+      .then((xdd) => {
+        if (xdd?.type === "public-key") {
+          return (xdd.response as AuthenticatorAssertionResponse).signature;
+        }
+      });
   } catch (err) {
     console.log("Error with scan", err);
   }
